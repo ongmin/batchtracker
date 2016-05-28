64ea1f549ee6830e4189400f846bbecef3bf5161
Min Ong <xinmin.ong@gmail.com>
2016-05-28 19:40:27 +0800

    Initial load working

--- app/components/Customer/filterableBatchRecordsTable.js
+++ app/components/Customer/filterableBatchRecordsTable.js
@@ -10,21 +10,47 @@ var BatchRecordsTable = require('./batchRecordsTable')
 // Store's state changes due to new result received by the action
 // The View re-renders by listening to Store's change
 
+var batchRecordsEndpoint = '/api/batchrecords'
+
 var filterableBatchRecordsTable = React.createClass({
+  getInitialState: function () {
+    return {
+      queryBatchNumber: '',
+      batchRecords: []
+    }
+  },
+  loadDataFromServer: function () {
+    $.ajax({
+      url: batchRecordsEndpoint,
+      dataType: 'json',
+      type: 'GET',
+      cache: false,
+      success: function (data) {
+        console.log(data)
+        this.setState({batchRecords: data})
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(batchRecordsEndpoint, status, err.toString())
+      }
+    })
+  },
+  componentDidMount: function () {
+    this.loadDataFromServer()
+  },
+  handleInputChange: function (e) {
+    this.setState({queryBatchNumber: e.target.value})
+  },
   render: function () {
     return (
             <div>
-              <SearchBar />
+              <SearchBar
+                value={this.state.queryBatchNumber}
+                onChange={this.handleInputChange}/>
               <BatchRecordsTableHeader />
-              <BatchRecordsTable batchRecords={this.state.batchRecords}/>
+              <BatchRecordsTable
+                batchRecords={this.state.batchRecords}/>
             </div>
-          ) },
-    getInitialState: function() {
-      return {
-        queryBatchNumber: '',
-        batchRecords: []
-      }
-    }
+  ) }
 
 })
 
--- app/components/Customer/searchBar.js
+++ app/components/Customer/searchBar.js
@@ -1,15 +1,25 @@
 var React = require('react')
-import AppActions from '../../actions/app-actions'
+// import AppActions from '../../actions/app-actions'
 var SearchBarButton = require('./searchBarButton')
 
 // Takes in an input queryBatchNumber
 // Rewrite this as stateless functional component and remove this comment
 var searchBar = React.createClass({
+  getInitialState: function () {
+    return {queryBatchNumber: ''}
+  },
+  handleInputChange: function (e) {
+    this.setState({queryBatchNumber: e.target.value})
+  },
   render: function () {
     return (
       <div>
         <form>
-          <input type='text' placeholder='Batch Number' value={this.props.queryBatchNumber}/>
+          <input
+            type='text'
+            placeholder='Batch Number'
+            value={this.state.queryBatchNumber}
+            onChange={this.handleInputChange} />
         </form>
         <SearchBarButton txt='Search'/>
       </div>
