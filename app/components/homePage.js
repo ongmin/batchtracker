c7b8c0cb80b0df235a402d0903b35cd52835341d
Min Ong <xinmin.ong@gmail.com>
2016-06-03 22:09:36 +0800

    Updated Frontend CSS

--- app/components/homePage.js
+++ app/components/homePage.js
@@ -5,12 +5,11 @@ var FilterableBatchRecordsTable = require('./customer/filterableBatchRecordsTabl
 var Home = React.createClass({
   render: function () {
     return (
+
         <div className='contentContainer'>
-          <div className='contentContainer-background'>
           <h2>Welcome to Paula&#39;s Choice Singapore</h2>
           <FilterableBatchRecordsTable />
-          </div>
-      </div>
+        </div>
     )
   }
 })
