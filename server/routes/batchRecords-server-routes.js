d8665178c959eeca9769209608b670b1bb0764dc
sevresbabylone <cheryl.francais@gmail.com>
2016-06-22 04:08:09 +0800

    switches out callbacks to promises in mongoose

--- server/routes/batchRecords-server-routes.js
+++ server/routes/batchRecords-server-routes.js
@@ -1,113 +1,109 @@
-var BatchRecord = require('./../models/batchRecord.js')
+const BatchRecord = require('./../models/batchRecord.js')
 
 module.exports = function (app, moltin) {
   app.route('/api/batchRecords')
-  .get(function (req, res) {
-    BatchRecord.find({}, function (err, batchRecords) {
-      if (err) throw err
-      console.log(batchRecords)
+  .get((req, res) => {
+    BatchRecord.find({}).exec()
+    .then(batchRecords => {
       res.json(batchRecords)
+    })
+    .catch((error) => {
+      console.log(error)
     })
   })
 
   app.route('/api/protected/batchRecords')
-  .post(function (req, res) {
+  .post((req, res) => {
     var newBatchRecord = new BatchRecord()
-    moltin.Product.Find({ sku: req.body.skuNumber }, function (product) {
+    moltin.Product.Find({ sku: req.body.skuNumber }, product => {
       newBatchRecord.productName = product[0].title
       newBatchRecord.batchNumber = req.body.batchNumber
       newBatchRecord.skuNumber = req.body.skuNumber
       newBatchRecord.expiryDate.month = req.body.month
       newBatchRecord.expiryDate.year = req.body.year
-      console.log(product[0].title)
 
-      // Save the newBatchRecord
-      newBatchRecord.save(err => {
-        if (err) {
-          if(err.code === 11000) {
-            res.status(409).send('Record is already in the database.')
-          }
-          else {
-            var errors = []
-            for (var property in err.errors) {
-              console.log(err.errors[property].message)
-                errors.push(err.errors[property].message)
-            }
-              res.status(400).json(errors)
-          }
-          return
-        }
-      // then return the whole database
-        BatchRecord.find({}, function (err, batchRecords) {
-          if (err) throw err
-          res.json(batchRecords)
-        })
-        console.log('batch record created!')
+      newBatchRecord.save()
+      .then(batchRecord => {
+        console.log(batchRecord._id + ' created!')
+        return BatchRecord.find({}).exec()
       })
-    }, function (error) {
-      console.log(error)
-      // Something went wrong...
-    })
-  })
-  // .post(batchRecords.create)
-
-  app.route('/api/protected/batchRecords/:id')
-  .delete(function (req, res) {
-    BatchRecord.findByIdAndRemove(req.params.id)
-    .exec(function (err, data) {
-      if (err) return console.error(err)
-      BatchRecord.find({}, function (err, batchRecords) {
-        if (err) throw err
-        console.log(batchRecords)
+      .then(batchRecords => {
         res.json(batchRecords)
       })
+      .catch(error => {
+        console.log(error)
+        if (error.code === 11000) {
+          res.status(409).send('Record is already in the database.')
+        } else {
+          var errors = []
+          for (var property in error.errors) {
+            console.log(error.errors[property].message)
+            errors.push(error.errors[property].message)
+          }
+          res.status(400).json(errors)
+        }
+      })
+    })
+  })
+  app.route('/api/protected/batchRecords/:id')
+  .delete(function (req, res) {
+    BatchRecord.findByIdAndRemove(req.params.id).exec()
+    .then((batchRecord) => {
+      console.log(batchRecord._id + ' deleted')
+      return BatchRecord.find({}).exec()
+    })
+    .then((batchRecords) => {
+      res.json(batchRecords)
+    })
+    .catch(error => {
+      console.error(error)
     })
   })
   .put(function (req, res) {
-    console.log(req.body.id)
-    moltin.Product.Find({ sku: req.body.skuNumber }, function (product) {
+    moltin.Product.Find({ sku: req.body.skuNumber }, product => {
       BatchRecord.findByIdAndUpdate(req.body.id,
-        {$set: {  productName: product[0].title,
-                  batchNumber: req.body.batchNumber,
-                  skuNumber: req.body.skuNumber,
-                  'expiryDate.month': req.body.month,
-                  'expiryDate.year': req.body.year }},
-        {new: true, runValidators: true },
-        function (err, updatedBatchRecord) {
-          if (err) {
-            console.error(err)
-            if(err.code === 11000) {
-              res.status(409).send('Record is already in the database.')
+        { $set: {
+          productName: product[0].title,
+          batchNumber: req.body.batchNumber,
+          skuNumber: req.body.skuNumber,
+          'expiryDate.month': req.body.month,
+          'expiryDate.year': req.body.year }
+                },
+        {new: true, runValidators: true})
+        .then(batchRecord => {
+          console.log(batchRecord._id + ' updated!')
+          return BatchRecord.find({}).exec()
+        })
+        .then(batchRecords => {
+          res.json(batchRecords)
+        })
+        .catch(error => {
+          console.log(error)
+          if (error.code === 11000) {
+            res.status(409).send('Record is already in the database.')
+          } else {
+            var errors = []
+            for (var property in error.errors) {
+              console.log(error.errors[property].message)
+              errors.push(error.errors[property].message)
             }
-            else {
-              var errors = []
-              for (var property in err.errors) {
-                // console.log(err.errors[property].message)
-                  errors.push(err.errors[property].message)
-              }
-                res.status(400).json(errors)
-            }
-            return
+            res.status(400).json(errors)
           }
-
-          // then return the whole database
-            BatchRecord.find({}, function (err, batchRecords) {
-              if (err) throw err
-              res.json(batchRecords)
-            })
         })
     })
   })
 
   app.route('/api/batchRecords/:batchNumber')
-  .get(function (req, res) {
+  .get((req, res) => {
     BatchRecord.find()
     .where('batchNumber')
     .equals(req.params.batchNumber)
-    .exec(function (err, batchRecords) {
-      if (err) throw err
-      console.log(batchRecords)
+    .exec()
+    .then(batchRecords => {
       res.json(batchRecords)
+    })
+    .catch(error => {
+      console.error(error)
     })
   })
 }
